e41cfe8a73d90563522af738938a396edc488eb2
unknown <kmf6001@gmail.com>
2024-02-04 14:43:19 +0300

    'saymyname'

---
+++ saymyname/bin/saymyname.dart
@@ -0,0 +1,8 @@
+import 'package:saymyname/saymyname.dart' as saymyname;
+
+void main(List<String> arguments) {
+  print('=========');
+  print('| FAHAD |');
+  print('=========');
+
+}
---
+++ saymyname/test/saymyname_test.dart
@@ -0,0 +1,8 @@
+import 'package:saymyname/saymyname.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
